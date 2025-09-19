e609bbe738264543ec9a621988f4bbea67956cd6
soniarawat909 <rawatsonia909@gmail.com>
2025-09-19 14:47:15 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 
-export const BASE_URL = "https://shoppit-api-1.onrender.com/";
+export const BASE_URL = "https://shoppit-api-1.onrender.com";
 
 
 
